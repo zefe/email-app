3cae3af337fc3a1e287d231dfe7629397be14368
zefe <zefetorres01@gmail.com>
2018-09-22 15:38:48 -0500

    add midlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
-// import data from '../mail-data.json';
-// import data from '../schemas/index';
-
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers/index';
 import { Map as map } from 'immutable';
-// const initialState = data;
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-// const initialState = {
-//   data: {
-//     // ...data,
-//     entities: data.entities,
-//     inbox: data.result.inbox,
-//     search: [],
-//   },
-//   details: {
-//     visibility: false,
-//     emailId: null
-//   } 
-// }
 
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(logger)
+  )
 );
 
 
